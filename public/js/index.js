73bbf6edac5d430902e88de58a1337f44e4ad755
Tyler <tyler@geekingdoing.com>
2018-05-04 20:43:48 -0400

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: 'belinda',
+        text: 'To the glee team!'
+    });
+
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+    console.log('New message', message);
+});
--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'Jake',
+        text: 'New message',
+        createdAt: Date.now()
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage);
+    });
+
     socket.on('disconnect', () => {
         console.log('Client disconnected from server');
     });
